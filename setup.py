a290c95a39c86f639e713c21456af06bcc301b33
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2019-04-29 15:19:03 +0300

    new release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.0',
+    version='1.1.1',
 
     description='An NLP library for small Uralic languages such as Skolt Sami, Moksha and such',
     long_description=long_description,
