3aca02e09e71952728836e240d39c4d34357c791
Mika <mika.hamalainen@helsinki.fi>
2020-02-05 21:04:25 +0200

    commandline download

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["requests", "hfst", "mikatools>=0.0.6"],
+    install_requires=["requests", "hfst", "mikatools>=0.0.6", "argparse"],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
