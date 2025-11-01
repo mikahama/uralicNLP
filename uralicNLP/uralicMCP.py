dce383e4d7eda0952c67990a0ef41f65e6002dd8
Mika <mika@flyforpoints.com>
2025-11-01 14:40:28 +0200

    MCP server works

--- uralicNLP/uralicMCP.py
+++ uralicNLP/uralicMCP.py
@@ -8,25 +8,34 @@ mcp = FastMCP("Uralic Language Processor (UralicMCP)")
 @mcp.tool()
 def analyze_word(word: str, language_iso_code: str) -> list:
 	"""Analyzes a word in a supported language and returns a list of analysis. Each analysis consists of a lemma and morphological tags such as kissa+N+Sg+Nom"""
-	return [x[0] for x in uralicApi.analyze(word, language_iso_code)]
+	return [x[0] for x in uralicApi.analyze(word, language_iso_code.lower())]
 
 
 @mcp.tool()
 def morphological_segmentation(word: str, language_iso_code: str) -> list:
 	"""Splits a word into morphemes. Returns a list of lists given that sometimes a word can be understood in multiple ways. E.g. koiranikin koira, ni, kin"""
-	return uralicApi.segment(word, language_iso_code)
+	return uralicApi.segment(word, language_iso_code.lower())
 
 @mcp.tool()
 def inflect_word(inflection: str, language_iso_code: str)  -> list:
 	"""Inflects a lemma to a given form. The inflection parameter must have the entire morphological reading such as koira+N+Sg+Gen to output koiran. NB there can be multiple forms for the same inflection"""
-	return [x[0] for x in uralicApi.generate(inflection, language_iso_code)]
+	return [x[0] for x in uralicApi.generate(inflection, language_iso_code.lower())]
 
-@mcp.resource("dictionary://{lemma}/{source_language_iso}/{target_language_iso}")
-def dictionary_lookup(lemma:str, source_language_iso:str, target_language_iso) -> list:
-	"""Finds target language translations for a lemma in source language. Traget language can be None for translations in all languages. The language codes are ISO codes. NB these dictionaries are for endangered languages. They mostly have translations from an endangered language to Finnish, Russian or English. There are no dictionaries between two endangered languages."""
+@mcp.tool()
+def lemmatize(word: str, language_iso_code: str)  -> list:
+	"""Lemmatizes a word in a given language. For example, voin in Finnish returns voi and voida"""
+	return uralicApi.lemmatize(word, language_iso_code.lower())
+
+@mcp.tool()
+def dictionary_lookup(lemma:str, source_language_iso:str, target_language_iso:str):
+	"""Finds target language translations for a lemma in source language. Target language can also be set to "all" for translations in all languages. The language codes are ISO codes. It's recommended to get translations for all languages."""
+	if target_language_iso == "all" or target_language_iso is None:
+		target_language_iso = None
+	else:
+		target_language_iso = target_language_iso.lower()
 	return uralicApi.get_translation(lemma, source_language_iso, target_language_iso)
 
-@mcp.resource()
+@mcp.tool()
 def list_supported_languages() -> dict:
 	"""Lists supported languages, their names and ISO codes. Use this if you don't know the ISO code.  """
 	sup = {x: [(z, string_processing.iso_to_name(z)) for z in y ] for x, y in uralicApi.supported_languages().items() }
